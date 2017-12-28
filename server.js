3d6382338af853d8ebcb7023b693a3f107c13580
Suélio <suelio.pontes@hotmail.com>
2017-12-28 07:29:27 -0200

    hotfix 01

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500)
     .json({
         status: 'error',
-        message: err.message
+       message: err.message
     });
 });
 
